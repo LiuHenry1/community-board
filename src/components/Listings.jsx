9fb165ceab21a2250fa1a00305cef910fc0d0be6
Henry Liu <hliu4176@gmail.com>
2023-09-16 20:50:59 -0500

    Implement Listings component

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -1,6 +1,13 @@
+import Card from "./Card";
 
-const Listings = () => {
-
+const Listings = ({entries}) => {
+    const cards = entries.map(entry => 
+        <Card imgSrc={entry.imgSrc} name={entry.name} url={entry.url} />);
+    return (
+        <div id="container">
+            {cards}
+        </div>
+    )
 }
 
 export default Listings;
