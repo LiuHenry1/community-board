1bffe5a2ae49bea3b341f3641cbc3ab61814c37f
Henry Liu <hliu4176@gmail.com>
2023-09-16 20:46:49 -0500

    Implement Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,11 @@
-const Card = () => {
-
+const Card = (entry) => {
+    return (
+        <div className="Card">
+            <img src={entry.imgSrc} />
+            <h4>{entry.name}</h4>
+            <a href={entry.url}>View Site</a>
+        </div>
+    )
 }
 
 export default Card;
